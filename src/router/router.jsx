891ff330c11bc7df922edf9ed19c8ef79326bbac
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-07 16:27:31 -0300

    Module 02-Aula 02

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,28 @@
 import { createBrowserRouter } from "react-router-dom"
-import PaginaInicial from "../paginas/PaginaInicial";
+import PaginaInicial from "../paginas/PaginaInicial"
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        children: [
+            {
+                path:'cadastro',
+                element: <h1>Selecão de tipo de cliente</h1>,
+                children: [
+                    {
+                        path: 'cliente',
+                        element: <h1>Interesesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluído</h1>
+                    }
+                ]
+            }
+        ],
     },
 ]);
